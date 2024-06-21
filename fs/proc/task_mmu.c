bb994e94bf3f3b62aa88ea52d315c6416901ad8a
Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>
2024-06-21 16:23:25 +0800

    treewide: Import KSU-Next + SusFS
    KernelSU: import v0.9.5
    KernelSU: fix su apps for encrypted /data
    KernelSU: hardcode KSU version
    fs: add KernelSU hooks
    drivers: input: add KernelSU hooks
    BACKPORT: fs/namespace.c path_umount
    defconfig: enable KSU
    KernelSU: break link to drivers/kernelsu
    KernelSU: update to v1.0.2
    kernelsu: Improve seccomp disable && Add locking to protect cred modifications in escape_to_root
    ksu: Add fallback if user didn't implement 'get_cred_rcu'
    BACKPORT: cred: add get_cred_rcu()
    ksu: fix: reset ambient capabilities to resolve D-Bus permission issues
    drivers: staging: ksu: NEXT v2 signature size/hash added
    ksu: Use global selinux state
    ksu: Added support custom version userspace
    ksu: [Fix] grant root to the shell in debug mode
    ksu: Remove conditional for ksu_umount_mnt
    kernel: import latest susfs4ksu (2025-01-08)
    ksu: add latest susfs (2025-01-08)
    defconfig: configure susfs
    fs: fix build without susfs_sus_path
    fs: add path_umount() to header
    ksu: hardcode version to 12262
    drivers: staging: ksu: Add support for custom signatures
    ksu: hardcode version
    /proc/pid/maps: dont allow paths with lineage to be listed
    /proc/pid/maps: fake all lineage
    /proc/pid/map_files: fake all lineage symlinks
    fs: pull updated susfs
    defconfig: disable sus path
    susfs: update to v1.5.4
    KernelSU: Add devpts hook calls manually
    kernelsu: Avoid unnecessarily overriding capabilities (#2381)
    kernelsu: clear securebits
    BACKPORT: maccess: rename strncpy_from_unsafe_user to strncpy_from_user_nofault
    kernelsu: handle optional backports
    susfs: Fix normal mounts that are carelessly hidden in mountinfo
    susfs: Remove redundant inode_permission check
    susfs: Log try_umount for default ksu mounts as well
    susfs: Bump version to 1.5.5
    
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -21,6 +21,9 @@
 #include <linux/mm_inline.h>
 #include <linux/ctype.h>
 #include <linux/io_record.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+#include <linux/susfs_def.h>
+#endif
 
 #include <asm/elf.h>
 #include <asm/tlb.h>
@@ -372,6 +375,27 @@ static void show_vma_header_prefix(struct seq_file *m,
 		   MAJOR(dev), MINOR(dev), ino);
 }
 
+static void show_vma_header_prefix_fake(struct seq_file *m,
+				   unsigned long start, unsigned long end,
+				   vm_flags_t flags, unsigned long long pgoff,
+				   dev_t dev, unsigned long ino)
+{
+	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
+	seq_printf(m, "%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu ",
+		   start,
+		   end,
+		   flags & VM_READ ? 'r' : '-',
+		   flags & VM_WRITE ? '-' : '-',
+		   flags & VM_EXEC ? '-' : '-',
+		   flags & VM_MAYSHARE ? 's' : 'p',
+		   pgoff,
+		   MAJOR(dev), MINOR(dev), ino);
+}
+
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+extern void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);
+#endif
+
 static void
 show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 {
@@ -386,9 +410,29 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+		if (unlikely(inode->i_state & INODE_STATE_SUS_KSTAT)) {
+			susfs_sus_ino_for_show_map_vma(inode->i_ino, &dev, &ino);
+			goto bypass_orig_flow;
+		}
+#endif
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+bypass_orig_flow:
+#endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+		struct dentry *dentry = file->f_path.dentry;
+		if (dentry) {
+			const char *path = (const char *)dentry->d_name.name;
+			if (strstr(path, "lineage")) {
+				start = vma->vm_start;
+				end = vma->vm_end;
+				show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
+				name = "/system/framework/framework-res.apk";
+				goto done;
+			}
+		}
 	}
 
 	start = vma->vm_start;
